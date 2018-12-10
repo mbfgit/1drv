cd39a1d4b0b2cfb3a7f0236a884e01db6caeda44
mbfgit <martinf@gmail.com>
2018-12-10 18:58:04 +0000

    Update wxt-ecm.js

--- wxt-ecm.js
+++ wxt-ecm.js
@@ -1,63 +1,66 @@
-        function getInfo(filesinfo) {
-            var msg = [];
-            for(var i=0;i<filesinfo.value.length;i++) {
-                msg.push({name: filesinfo.value[i].name, uri: filesinfo.value[i].webUrl,
-                    fileId: filesinfo.value[i].id, driveId: filesinfo.value[i].parentReference.driveId,
-                    driveType: filesinfo.value[i].parentReference.driveType,
-                    mimeType: filesinfo.value[i].file.mimeType, size: filesinfo.value[i].size});
-            }
-            return msg;
+function getInfo(filesinfo) {
+    var msg = [];
+    for(var i=0;i<filesinfo.value.length;i++) {
+        msg.push({name: filesinfo.value[i].name, uri: filesinfo.value[i].webUrl,
+            fileId: filesinfo.value[i].id, driveId: filesinfo.value[i].parentReference.driveId,
+            driveType: filesinfo.value[i].parentReference.driveType,
+            mimeType: filesinfo.value[i].file.mimeType, size: filesinfo.value[i].size});
+    }
+    return msg;
+}
+var oneDriveFilePickerError =
+    function ( e ) {
+        if (window.ecmError) {
+            window.ecmError(JSON.stringify(e, null, 4));
         }
-        var oneDriveFilePickerError = 
-            function ( e ) {
-                if (window.ecmError) {
-                    window.ecmError(JSON.stringify(e, null, 4));
-                }
-            }
-        var oneDriveFilePickerSuccess = 
-            function ( filesinfo ) {
-                // OSX + iOS
-                if(window.webkit && window.webkit.messageHandlers.ecmShare) {
-                    var msg = { files : getInfo(filesinfo)};
-                    window.webkit.messageHandlers.ecmShare.postMessage(msg);
-                }
-                // CEF
-                if (window.ecmShare) {
-                    var msg = { files : getInfo(filesinfo)};
-                    window.ecmShare(JSON.stringify(msg,null,2));
-                }
-            }
-        var oneDriveFilePickerCancel =
-            function ( ) {
-                showResults('OneDrive Picker Cancelled');
-                if (window.ecmCancel) {
-                    window.ecmCancel();
-                }
-            }
-        function getOptions(action, clientId) {
-            var odOptions = {
-                clientId: clientId,
-                action: action,
-                multiSelect: true,
-                openInNewWindow: true,
-                success: function(files) {
-                    oneDriveFilePickerSuccess(files);
-                },
-                cancel: function() {
-                    oneDriveFilePickerCancel();
-                },
-                error: function(e) {
-                    oneDriveFilePickerError(e);
-                },
-                redirectUri: window.location.href,
-                advanced: {
-                    queryParameters: "select=id,name,size,file,folder,webUrl,parentReference"
-                }
-            };
-            return odOptions;
+    }
+var oneDriveFilePickerSuccess =
+    function ( filesinfo ) {
+        // OSX + iOS
+        if(window.webkit && window.webkit.messageHandlers.ecmShare) {
+            var msg = { files : getInfo(filesinfo)};
+            window.webkit.messageHandlers.ecmShare.postMessage(msg);
         }
-        function launchOneDrivePicker(clientId,action) {
-            action = action || "query";
-            var odOptions = getOptions(action, clientId);
-            OneDrive.open( odOptions );
+        // CEF
+        if (window.ecmShare) {
+            var msg = { files : getInfo(filesinfo)};
+            window.ecmShare(JSON.stringify(msg,null,2));
         }
+    }
+var oneDriveFilePickerCancel =
+    function ( ) {
+        if (window.ecmCancel) {
+            window.ecmCancel();
+        }
+    }
+function getOptions(action, clientId, loginHint, isConsumerAccount) {
+    var odOptions = {
+        clientId: clientId,
+        action: action,
+        multiSelect: true,
+        openInNewWindow: true,
+        success: function(files) {
+            oneDriveFilePickerSuccess(files);
+        },
+        cancel: function() {
+            oneDriveFilePickerCancel();
+        },
+        error: function(e) {
+            oneDriveFilePickerError(e);
+        },
+        redirectUri: window.location.href,
+        advanced: {
+            queryParameters: "select=id,name,size,file,folder,webUrl,parentReference,sharepointIds&expand=thumbnails"
+        }
+    };
+    if(typeof loginHint !== "undefined" && typeof isConsumerAccount !== "undefined") {
+        odOptions.advanced.loginHint = loginHint;
+        odOptions.advanced.isConsumerAccount = isConsumerAccount;
+    }
+    return odOptions;
+}
+function launchOneDrivePicker(clientId,loginHint,isConsumerAccount,action) {
+    action = action || "query";
+    var odOptions = getOptions(action, clientId, loginHint, isConsumerAccount);
+    OneDrive.open( odOptions );
+}
