64a9777ab9231e4f383c77fd50170e7991150e2b
mbfgit <martinf@gmail.com>
2018-12-12 17:07:25 +0000

    Update wxt-ecm.js

--- wxt-ecm.js
+++ wxt-ecm.js
@@ -1,57 +1,60 @@
 function getInfo(filesinfo) {
     var msg = [];
     for(var i=0;i<filesinfo.value.length;i++) {
-        msg.push({name: filesinfo.value[i].name, uri: filesinfo.value[i].webUrl,
-            fileId: filesinfo.value[i].id, driveId: filesinfo.value[i].parentReference.driveId,
-            driveType: filesinfo.value[i].parentReference.driveType,
-            mimeType: filesinfo.value[i].file.mimeType, size: filesinfo.value[i].size});
+	msg.push({name: filesinfo.value[i].name, uri: filesinfo.value[i].webUrl,
+	    fileId: filesinfo.value[i].id, driveId: filesinfo.value[i].parentReference.driveId,
+	    driveType: filesinfo.value[i].parentReference.driveType,
+	    mimeType: filesinfo.value[i].file.mimeType, size: filesinfo.value[i].size});
     }
     return msg;
 }
-var oneDriveFilePickerError =
+var oneDriveFilePickerError = 
     function ( e ) {
-        if (window.ecmError) {
-            window.ecmError(JSON.stringify(e, null, 4));
-        }
+	if (window.ecmError) {
+	    window.ecmError(JSON.stringify(e, null, 4));
+	}
     }
-var oneDriveFilePickerSuccess =
+var oneDriveFilePickerSuccess = 
     function ( filesinfo ) {
-        // OSX + iOS
-        if(window.webkit && window.webkit.messageHandlers.ecmShare) {
-            var msg = { files : getInfo(filesinfo)};
-            window.webkit.messageHandlers.ecmShare.postMessage(msg);
-        }
-        // CEF
-        if (window.ecmShare) {
-            var msg = { files : getInfo(filesinfo)};
-            window.ecmShare(JSON.stringify(msg,null,2));
-        }
+	// OSX + iOS
+	if(window.webkit && window.webkit.messageHandlers.ecmShare) {
+	    var msg = { files : getInfo(filesinfo)};
+	    window.webkit.messageHandlers.ecmShare.postMessage(msg);
+	}
+	// CEF
+	if (window.ecmShare) {
+	    var msg = { files : getInfo(filesinfo)};
+	    window.ecmShare(JSON.stringify(msg,null,2));
+	}
     }
 var oneDriveFilePickerCancel =
     function ( ) {
-        if (window.ecmCancel) {
-            window.ecmCancel();
-        }
+	if (window.ecmCancel) {
+	    window.ecmCancel();
+	}
     }
-function getOptions(action, clientId, loginHint, isConsumerAccount) {
+function getOptions(clientId, loginHint, isConsumerAccount, action) {
     var odOptions = {
-        clientId: clientId,
-        action: action,
-        multiSelect: true,
-        openInNewWindow: true,
-        success: function(files) {
-            oneDriveFilePickerSuccess(files);
-        },
-        cancel: function() {
-            oneDriveFilePickerCancel();
-        },
-        error: function(e) {
-            oneDriveFilePickerError(e);
-        },
-        redirectUri: window.location.href,
-        advanced: {
-            queryParameters: "select=id,name,size,file,folder,webUrl,parentReference,sharepointIds&expand=thumbnails"
-        }
+	clientId: clientId,
+	action: action,
+	multiSelect: true,
+	openInNewWindow: true,
+	success: function(files) {
+	    oneDriveFilePickerSuccess(files);
+	},
+	cancel: function() {
+	    oneDriveFilePickerCancel();
+	},
+	error: function(e) {
+	    oneDriveFilePickerError(e);
+	},
+	redirectUri: window.location.href,
+	advanced: {
+		queryParameters: "select=id,name,size,file,folder,webUrl,parentReference",
+		navigation: {
+			sourceTypes: ["OneDrive", "Sites"]
+		}
+	}
     };
     if(typeof loginHint !== "undefined" && typeof isConsumerAccount !== "undefined") {
         odOptions.advanced.loginHint = loginHint;
@@ -59,8 +62,8 @@ function getOptions(action, clientId, loginHint, isConsumerAccount) {
     }
     return odOptions;
 }
-function launchOneDrivePicker(clientId,loginHint,isConsumerAccount,action) {
+function launchOneDrivePicker(clientId, loginHint, isConsumerAccount, action) {
     action = action || "query";
-    var odOptions = getOptions(action, clientId, loginHint, isConsumerAccount);
+    var odOptions = getOptions(clientId, loginHint, isConsumerAccount, action);
     OneDrive.open( odOptions );
 }
